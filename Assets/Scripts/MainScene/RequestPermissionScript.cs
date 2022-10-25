79fc4ba41ad5847744100500f1da9cdf3d652f87
sNadehito <39584095+sNadehito@users.noreply.github.com>
2022-10-25 10:03:32 +0700

    init commit

---
+++ Assets/Scripts/MainScene/RequestPermissionScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.SceneManagement;
+
+public class RequestPermissionScript : MonoBehaviour
+{ 
+
+    void Start()
+    {
+        Permission.RequestUserPermission(Permission.Camera);
+    }
+
+    void OnApplicationFocus(bool focus)
+    {
+        if (Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            Debug.Log("Permission Allowed");
+            // Delete comment later
+            // SceneManager.LoadScene("AR");
+        }
+        else
+        {
+            Debug.Log("Permission Denied");
+        }
+    }
+
+}
---
+++ Assets/Scripts/MainScene/Soldier.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier : MonoBehaviour
+{
+
+    [SerializeField] bool isActivate;
+    [SerializeField] bool isPlayer;
+    [SerializeField] bool isAttacker;
+    [SerializeField] bool isHoldBall;
+
+    public bool IsActivate
+    {
+        get
+        {
+            return IsActivate;
+        }
+        set
+        {
+            isActivate = value;
+            if (!value)
+            {
+                ChangeColor(Color.gray);
+                return;
+            }
+            if (!isPlayer)
+            {
+                ChangeColor(Color.red);
+                return;
+            }
+            ChangeColor(Color.blue);
+        }
+    }
+
+    void ChangeColor(Color color)
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = color;
+    }
+}
